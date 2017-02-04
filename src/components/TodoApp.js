e78e535e28bec876d59d7144e7deaeb65eba3d77
Daesung Kim <daesung.kim1@yahoo.com>
2017-02-04 12:13:23 +0900

    #6 lesson 18 add toggle todo

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -20,7 +20,20 @@ export default class TodoApp extends Component {
         </button>
         <ul>
           {todos.map(todo =>
-            <li key={todo.id}>
+            <li
+              key={todo.id}
+              onClick={() => {
+                store.dispatch({
+                  type: 'TOGGLE_TODO',
+                  id: todo.id
+                })
+              }}
+              style={{
+                textDecoration:
+                  todo.completed ?
+                  'line-through' :
+                  'none'
+              }}>
               {todo.text}
             </li>
           )}
