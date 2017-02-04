238d53fe19b3f03db1dcd1a7bdb7422926b8816f
Daesung Kim <daesung.kim1@yahoo.com>
2017-02-04 16:18:04 +0900

    #14 lesson 27 Generating Containers with connect() from React Redux (VisibleTodoList)

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,44 +1,29 @@
-import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import TodoList from '../components/TodoList'
 
-class VisibleTodoList extends Component {
-
-  componentDidMount() {
-    const {store} = this.context
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    )
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe()
-  }
-
-  render() {
-    const props = this.props
-    const { store } = this.context
-    const state = store.getState()
-    return (
-      <TodoList
-        todos={
-          props.getVisibleTodos(
-            state.todos,
-            state.visibilityFilter
-          )
-        }
-        onTodoClick={id=>
-          store.dispatch({
-            type:'TOGGLE_TODO',
-            id
-          })
-        }
-      />
+const mapStateToProps = (state, ownProps) => {
+  return {
+    todos: ownProps.getVisibleTodos(
+      state.todos,
+      state.visibilityFilter
     )
   }
 }
 
-VisibleTodoList.contextTypes = {
-  store: React.PropTypes.object
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: id => {
+      dispatch({
+        type:'TOGGLE_TODO',
+        id
+      })
+    }
+  }
 }
+
+const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
 
 export default VisibleTodoList
