ded3c852d8f37a6511502d195868c5222feefc80
Daesung Kim <daesung.kim1@yahoo.com>
2017-02-06 12:24:19 +0900

    #28 13) Adding a Fake Backend to the Project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import Root from './components/Root'
 import configureStore from './configureStore'
+import { fetchTodos } from './api';
+
+fetchTodos('all').then(todos =>
+  console.log(todos)
+)
 
 const store = configureStore()
-
-ReactDOM.render(
+render(
   <Root store={store} />,
   document.getElementById('root')
 );
