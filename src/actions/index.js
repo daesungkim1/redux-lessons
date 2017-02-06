89f27d082daa8d9b9a8acb0dca9abefb7f165f33
Daesung Kim <daesung.kim1@yahoo.com>
2017-02-06 19:02:15 +0900

    #41 26) Normalizing API Responses with normalizr

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { normalize } from 'normalizr'
+import * as schema from './schema'
 import { getIsFetching } from '../reducers'
 import * as api from '../api'
 
@@ -15,7 +17,7 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
     dispatch({
       type: 'FETCH_TODOS_SUCCESS',
       filter,
-      response,
+      response: normalize(response, schema.arrayOfTodos),
     })
   },
   error => {
@@ -31,7 +33,7 @@ export const addTodo = (text) => (dispatch) =>
   api.addTodo(text).then(response => {
     dispatch ({
       type: 'ADD_TODO_SUCCESS',
-      response
+      response: normalize(response, schema.todo)
     })
   })
 
